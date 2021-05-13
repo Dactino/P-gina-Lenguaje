6e498bd1fccd16ed47a5021623794a344409cb5f
Dactino <dani1802.martin@gmail.com>
2021-05-13 17:53:45 +0200

    En principio con diseño responsivo

---
+++ logo.cc
@@ -0,0 +1,15 @@
+#imagen {
+    content: url(logoPri.jpg);
+    height: 40vh;
+    width: 50%;
+    margin-left: 25%;
+    margin-top: 10%;
+}
+   
+#imagen:hover {
+    content: url(logoSec.jpg);
+    height: 40vh;
+    width: 50%;
+    margin-left: 25%;
+    margin-top: 10%;
+}
